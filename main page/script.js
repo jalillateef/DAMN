8a2e66166fcf69a50a0520cade70ba81102839e1
jlateef <jlateef808@gmail.com>
2021-07-09 17:00:12 -0400

    updated header

--- main page/script.js
+++ main page/script.js
@@ -6,16 +6,24 @@ const body = document.querySelector('body')
 // * an event listener for a submit within the main page
 body.addEventListener('submit', function(e) {
     e.preventDefault()
+    // window.location.href("../results/searchResult.html")
+    window.open("../results/searchResult.html")
     // console.log('test')
     const searchString = search.value
     const urlEncodedSearchString = encodeURIComponent(searchString)
     // console.log(`http://musicbrainz.org/ws/2/artist/?query=artist:${urlEncodedSearchString}`)
-    fetch (`http://musicbrainz.org/ws/2/artist/?query=artist:${urlEncodedSearchString}`)
+    fetch (`http://musicbrainz.org/ws/2/artist/?fmt=json&query=artist:${urlEncodedSearchString}`)
     .then((res) => {
         return res.json()
     })
     .then((data) => {
-        const artistName = data
-        console.log(artistName)
+        const allArtist = data.artists
+        // console.log(allArtist)
+        // this map allows me to get a specification for all artist that return
+        // can use this map to grab all artist id, basic info
+        allArtist.map((artist) => {
+            const artistID = artist.id
+            // console.log(artistID)
+        })
     })
 })
