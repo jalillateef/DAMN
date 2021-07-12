91e8e2e64284973f0c0df5146b821caea9263503
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-07-11 19:42:40 -0500

    added more functionality to javascript, can display the right amount of boxes needed for each artist

--- results/scriptSearch.js
+++ results/scriptSearch.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){    
-    let artistIDSJSON = localStorage.getItem("artistIDS")
-    let artistIDS = JSON.parse(artistIDSJSON)
-    console.log('test')
-    console.log(artistIDS)
+    let artistsInfoJSON = localStorage.getItem("artistsInfo")
+    let artistsInfo = JSON.parse(artistsInfoJSON)
+    console.log(artistsInfo)
+    const container = document.querySelector(".all-artist")
+    artistsInfo.map((data) => {
+
+        container.innerHTML = data.map((artist) => {
+            const artistID = artist.id
+            console.log(artistID)
+            return `<div class='artist-container'>
+                        <div class='artist-picture me-3'>
+                        <img src="pictures/damn-placeholder.png" alt="..." class="img-thumbnail">
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                        <h1 class='artist-text'>${artist.name}</h1>
+                        <p class='artist-info'>Information</p>
+                    </div>
+                </div>
+            </div>`
+        }).join("")
+    })
 })
