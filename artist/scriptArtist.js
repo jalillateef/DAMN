aae35aedda2d1a7cbfce20a18cc70e7e75a3677e
Dylan Guidry <dylanguidry20@gmail.com>
2021-07-14 11:36:30 -0500

    added event info but still need to do some css

--- artist/scriptArtist.js
+++ artist/scriptArtist.js
@@ -1,10 +1,11 @@
 console.log('anything')
 const container = document.getElementById('artistContent')
 
-    const query=new URLSearchParams(window.location.search)
-    const artistID = (query.get('artist'))
-    const urlEncodedSearchString = encodeURIComponent(artistID)
-    fetch (`https://musicbrainz.org/ws/2/artist/${artistID}?inc=url-rels+releases+works&fmt=json`)
+
+const query = new URLSearchParams(window.location.search)
+const artistID = (query.get('artist'))
+const urlEncodedSearchString = encodeURIComponent(artistID)
+fetch(`https://musicbrainz.org/ws/2/artist/${artistID}?inc=url-rels+releases+works&fmt=json`)
     .then((res) => {
         return res.json()
     })
@@ -12,28 +13,42 @@ const container = document.getElementById('artistContent')
         const artistName = mbdata.name
         const urlEncodedArtist = encodeURIComponent(artistName)
         fetch(`https://rest.bandsintown.com/artists/${urlEncodedArtist}?app_id=0c3d7989425512a2b6dea2004f6cdd51`)
-        .then((res) => {
-            return res.json()
-        })
-        .then((bitdata) => {
-            console.log(mbdata)
-            console.log(bitdata)
-            console.log(container)
-            container.innerHTML =
-                ( `<img src="${bitdata.image_url}" alt="..." class="img-thumbnail">
-                <h1 class='header1'><b>Artist Name:</b> ${mbdata.name} </h1>
-                <h1 class='header1'><b>Artist Hometown:</b> ${mbdata['begin_area'].name} </h1>
-                <h1 class='header1'><b>Artist Birth:</b> ${mbdata['life-span'].begin} </h1>
-                <h1 class='header1'><b>Artist Genre:</b> Genre Here </h1>
-                <h2 class= 'header2'>More general information.</h2>`)
-        })
-        // console.log(data.name)
-        // console.log(container)
-        // container.innerHTML =
-        //     ( `<img src="./pictures/record.gif" alt="..." class="img-thumbnail">
-        //     <h1 class='header1'><b>Artist Name:</b> ${data.name} </h1>
-        //     <h1 class='header1'><b>Artist Hometown:</b> ${data.begin_area.name} </h1>
-        //     <h1 class='header1'><b>Artist Birth:</b> ${data['life-span'].begin} </h1>
-        //     <h1 class='header1'><b>Artist Genre:</b> Genre Here </h1>
-        //     <h2 class= 'header2'>More general information.</h2>`)
+            .then((res) => {
+                return res.json()
+            })
+            .then((bitdata) => {
+                const urlEncodedEvents = encodeURIComponent(artistName)
+                fetch(`https://rest.bandsintown.com/artists/${urlEncodedEvents}/events?app_id=0c3d7989425512a2b6dea2004f6cdd51`)
+                    .then((res) => {
+                        return res.json()
+                    })
+                    .then((mbedata) => {
+                        console.log(mbdata)
+                        console.log(bitdata)
+                        console.log(mbedata)
+                        console.log(container)
+                        // Event html 
+                        const eventhtml =  mbedata.map((artistEvent) => {
+                            console.log(artistEvent.title)
+                            return (`
+                    <p class= 'eventdisplay'>${artistEvent.title}</p>
+                    <p class= 'eventtime' >${artistEvent.datetime}</p>`)
+                        }).join('')
+                        // Artist html
+                        container.innerHTML = `<img src="${bitdata.image_url}" alt="..." class="img-thumbnail">
+                        <h1 class='header1'><b>Artist Name:</b> ${mbdata.name} </h1>
+                        ${mbdata['begin-area'] ?`<h1 class="header1"><b>Artist Hometown:</b> ${mbdata['begin-area']['sort-name']}</h1>` :""}
+                        <h1 class='header1'><b>Artist Birth:</b> ${mbdata['life-span'].begin} </h1>
+                        <div class="card-body">
+                                <div class="events"> 
+                    <div class="card">
+                        <div class="card-body">
+                        ${eventhtml}
+                        </div>
+                        </div>
+                    </div>
+                            </div>
+                        `
+                    })
+            })
     })
